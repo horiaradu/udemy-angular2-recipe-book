3d29ac8d0a49118b364e714fff405f37738dd4ed
Horia Radu <horia.radu23@gmail.com>
2017-03-14 14:55:35 +0200

    use services

---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,16 @@
+import { Ingredient } from "../shared/ingredient";
+
+export class ShoppingListService {
+  private items: Ingredient[] = [];
+
+  constructor() {
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItems(ingredients: Ingredient[]) {
+    ingredients.forEach(item => this.items.push(item));
+  }
+}
