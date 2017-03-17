40b3bdd1cd45a433bc1d196ff72b5b9637501de5
Horia Radu <horia.radu23@gmail.com>
2017-03-17 19:34:35 +0200

    modules - lazy loading

---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { ShoppingListAddComponent } from "./shopping-list-add.component";
+import { ShoppingListComponent } from "./shopping-list.component";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+  declarations: [
+    ShoppingListComponent,
+    ShoppingListAddComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ]
+})
+export class ShoppingListModule {
+
+}
